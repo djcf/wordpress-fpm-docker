0927d997f64c40bb9b2f499af062e26e132bab6e
Daniel james <daniel.james@mailbox.org>
2017-02-10 14:30:33 +0000

    Transitioning to alpine-based system

--- Vagrantfile
+++ Vagrantfile
@@ -24,13 +24,9 @@ Vagrant.configure("2") do |config|
     ],
     run: "always"
 
-$COPY_SVC_UNITS = <<SH
-  cp /vagrant/systemd.services/mysql.service /etc/systemd/system/multi-user.target.wants
-  cp /vagrant/systemd.services/switchboard.service /etc/systemd/system/multi-user.target.wants
-  cp /vagrant/systemd.services/vhost.fpm.service /etc/systemd/system
-  cp /vagrant/systemd.services/vhost.fpm-waker.service /etc/systemd/system
-  cp /vagrant/systemd.services/vhost.fpm-waker.socket /etc/systemd/system
-SH
+  config.vm.provision "ansible" do |ansible|
+    ansible.verbose = "v"
+    ansible.playbook = "ansible/configure-host/configure-docker.yml"
+  end
 
-  config.vm.provision "shell", inline: $COPY_SVC_UNITS
 end
---
+++ scripts/renew-fpm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+    echo "Useage: $0 DOMAIN"
+    exit 1
+fi
+
+systemctl stop fpm@$1.service
+systemctl disable fpm@$1.service
+systemctl stop fpm-waker@$1.service
+systemctl disable fpm-waker@$1.service
+systemctl stop fpm-waker@$1.socket
+systemctl disable fpm-waker@$1.service
+rm -f /var/run/docker-apps/$1/*
+systemctl enable fpm-waker@$1.socket
+systemctl start fpm-waker@$1.socket
+
+# in fish:
+# function renewfpm
+#              systemctl stop fpm@$argv.service
+#              systemctl disable fpm@$argv.service
+#              systemctl stop fpm-waker@$argv.service
+#              systemctl disable fpm-waker@$argv.service
+#              systemctl stop fpm-waker@$argv.socket
+#              systemctl disable fpm-waker@$argv.service
+#              rm f /var/run/docker-apps/$argv/*
+#              systemctl enable fpm-waker@$argv.socket
+#              systemctl start fpm-waker@$argv.socket
+#          end

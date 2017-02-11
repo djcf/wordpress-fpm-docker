4b469572a8e641ce6f31fa0cce2cb6293604d199
Daniel james <daniel.james@mailbox.org>
2017-02-11 18:46:19 +0000

    SSL now working

--- scripts/renew-fpm.sh
+++ scripts/renew-fpm.sh
@@ -14,16 +14,3 @@ systemctl disable fpm-waker@$1.service
 rm -f /var/run/docker-apps/$1/*
 systemctl enable fpm-waker@$1.socket
 systemctl start fpm-waker@$1.socket
-
-# in fish:
- function renewfpm
-              systemctl stop fpm@$argv.service
-              systemctl disable fpm@$argv.service
-              systemctl stop fpm-waker@$argv.service
-              systemctl disable fpm-waker@$argv.service
-              systemctl stop fpm-waker@$argv.socket
-              systemctl disable fpm-waker@$argv.service
-              rm f /var/run/docker-apps/$argv/*
-              systemctl enable fpm-waker@$argv.socket
-              systemctl start fpm-waker@$argv.socket
-          end

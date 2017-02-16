0137a89360b8db67b41325c82b8963425592aeb6
Daniel james <daniel.james@mailbox.org>
2017-02-16 14:28:58 +0000

    Web host now shrinks image sizes

---
+++ ansible/roles/install-web/files/cron/shink-in-place.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Purpose: batch image resizer
+# Author: daniel
+
+# absolute path to image folder
+FOLDER=$(pwd)
+
+# max height
+MAX_WIDTH=1600
+
+# max width
+MAX_HEIGHT=1600
+
+MAX_SIZE=500000
+
+for f in $(find $FOLDER -iname '*.jpg' -o -iname '*.png' -iname '*.jpeg' -type f)
+do
+    mogrify -resize "$MAX_WIDTHx$MAX_HEIGHT>" $f
+	if [ $(stat --format %s $f) -ge $MAX_SIZE ]; then
+		mogrify -quality 60 $f
+	fi
+done

cf03deab652206192f9534ec5837b54799da0d6c
daniel <daniel@noflag.org.uk>
2017-06-08 14:20:18 +0000

    Update 'ansible/roles/install-web/files/bin/update-single-wordpress.sh'
    Update all wordpress bash script confirmed to work

--- ansible/roles/install-web/files/bin/update-single-wordpress.sh
+++ ansible/roles/install-web/files/bin/update-single-wordpress.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
-if [ "$#" -ne 2 ]; then
-    echo "Alter a wordpress container so that it uses a specific version of wordpress"
-    echo "This is broadly not possible with the group-fpm container"
-    echo "See https://hub.docker.com/_/wordpress for valid values"
-    echo "Must choose tags of base image wordpress-php-fpm"
-    echo "If --bleeding is the last arg, the latest version of WP is fetched from wordpress.org instead of docker hub"
-    echo "This is likely to be more recent than the official docker images"
-    echo ""
-    echo "Useage: $0 CONTAINER_NAME --bleeding"
-    echo "Example: ./update-single-wordpress.sh test --bleeding"
-    exit 1
-fi
-
 IMAGE=wordpress-php7.1-fpm-alpine-mod
-CONTAINER_NAME="$1"
-EXTRA_ARGS=""
-
-if [ "$2" == "--bleeding" ]; then
-	TAG="edge"
-	EXTRA_ARGS="--build-arg EDGE=TRUE"
+if [ -d /vagrant/docker ]; then
+	LATEST_WP_DIR=/vagrant/docker/apps/wordpress
+elif [ -d /usr/local/web ]; then
+	LATEST_WP_DIR=/usr/local/web/docker/apps/wordpress
+elif [ -d "$1" ]; then
+	LATEST_WP_DIR="$1"
+else
+	echo "Could not find installation directory containing docker image"
+	echo "Usage: $1 WP_DOCKER_IMAGE_DIRECTORY"
+	exit 0
 fi
 
-docker build $LATEST_WP_DIR -t $IMAGE:$TAG $EXTRA_ARGS
-NEW_VERSION=$(docker run --rm $IMAGE:$TAG wp --allow-root core version)
-OLD_VERSION=$(docker exec -it $CONTAINER_NAME.fpm sh -c 'wp --allow-root core version')
+CURRENT_VERSION=$(docker run --rm $IMAGE:latest wp --allow-root core version)
+docker rmi $IMAGE:old
+docker tag $IMAGE:latest $IMAGE:$CURRENT_VERSION
+docker tag $IMAGE:latest $IMAGE:old
+docker rmi $IMAGE:latest
+docker build --pull $LATEST_WP_DIR -t $IMAGE:latest
+LATEST_VERSION=$(docker run --rm $IMAGE:latest wp --allow-root core version)
+docker tag $IMAGE:latest $IMAGE:$LATEST_VERSION
+docker rm -f group-wordpress.fpm
+IFS=" "
+while read -r container; do
+	echo "Updating $container to $LATEST_VERSION"
+	SUBDOMAIN=${container::-4}
+	/usr/local/bin/renew-fpm-container.sh $SUBDOMAIN
+	docker run --env-file /var/www/$SUBDOMAIN/.env --rm --volumes-from $container --network=docker_sqlnet --link sqldb.noflag.org.uk:sqldb.noflag.org.uk $IMAGE:latest sh -c 'wp --allow-root core update-db; w
+p --allow-root plugin update --all'
+done <<< $(docker ps --all --filter ancestor=wordpress-php7.1-fpm-alpine-mod:old --format "{{.Names}}")
 
-echo "Updating $container ($OLD_VERSION) to $NEW_VERSION"
-/usr/local/bin/renew-fpm-container.sh $container
-docker run --env-file /var/www/$container/.env --rm --volumes-from $container.fpm $IMAGE:$TAG sh -c 'wp --allow-root core update-db; wp --allow-root plugin update --all'
-docker tag $IMAGE:$TAG $IMAGE:$NEW_VERSION
+#cd $LATEST_WP_DIR
+#cd ../../
+#docker-compose up -d wordpress
+#while read -r container; do
+#	echo "Updating $container to $LATEST_VERSION
+#	SUBDOMAIN=${container::-4}
+#	docker run --env-file /var/www/$SUBDOMAIN/.env --rm --volumes-from group-wordpress.fpm $IMAGE:latest sh -c 'wp --allow-root core update-db; wp --allow-root plugin update --all'
+#done <<< $(/usr/local/bin/view-php-group-websites.sh)

bc7977ac1c28bc628f6dad0c0a379639db5b438c
Daniel james <daniel.james@mailbox.org>
2018-06-20 22:30:28 +0100

    added convenience script for adding to the ansible inventory

---
+++ add-to-vagrant-inventory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+INVENTORY=.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory
+if [ $# -eq 0 ]; then
+	echo "Enter the host DNS to add to the ansible inventory."
+	read host
+else
+	host=$1
+fi
+if grep -q "$host" $INVENTORY; then
+	echo "master ansible_host=$host" >> $INVENTORY
+fi
